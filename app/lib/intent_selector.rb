11f798a81f0cffe85123b78e0a8466d836da69ae
Alex <1sasha.sasha1@gmail.com>
2018-09-27 12:27:05 +0200

    [refactor] Refactored Intent Selector class, removed odd code that should be inherited

--- app/lib/intent_selector.rb
+++ app/lib/intent_selector.rb
@@ -1,18 +1,7 @@
 require 'active_support/core_ext/string'
 class IntentSelector < RequestHandler
-  attr_reader :response
-  def initialize(request:)
-    @request = request
-    set_response
-  end
-
-  def self.get_response(request:)
-    new(request: request).response
-  end
 
   private
-  attr_reader :request
-
   def set_response
     @response = find_intent_class.get_response(request: request)
   end
